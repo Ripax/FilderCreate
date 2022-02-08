03f163c9f862d17cc09268f70b4bd1b5f7dfcb41
Ripax <ripanbiswas007@gmail.com>
2022-02-08 12:34:44 +0530

    test

--- test.py
+++ test.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python
-
-from PyQt5.QtCore import QDateTime, Qt
-
-now = QDateTime.currentDateTime()
-
-print('Local datetime: ', now.toString())
-print('Universal datetime: ', now.toUTC().toString(Qt.ISODate))
-
-print(f'The offset from UTC is: {now.offsetFromUtc()} seconds')
-
-
-
-
+# author : Htmldigger
+# Date : 4thfeb2022
+# ## #####################################################
+# Import Module here.
 import os
 import datetime
+from getpass import getuser
 
+# Added color to python terminal
+# print("\033[1;32m This text is Bright Green  \n")
+
+# UserName from system.
+uName = getuser().capitalize()
+
+# Now Time gather
 today = datetime.datetime.now()
-# dd/mm/YY
-folder = today.strftime("%B_%d-%m-%Y__%H-%M-%S")
 
-try:
-    os.makedirs(folder)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
+# dd/mm/YY
+folder = today.strftime("%B_%d-%m-%Y_{x}_%H-%M-%S").format(x=uName)
+
+if uName == 'Rion':
+    print("\033[1;32m************** Admin Panel **************")
+    print(f"\nWelcome Back Admin.\nDate : {today}\n\n*****************************************\n")
+    print('Creating your woring directory...')
+    try:
+        print(f'Hey {uName} welcome to HTMLDigger sucessfully created : {folder}')
+        # os.makedirs(folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    print("\033[0m\n")
+    print(folder)
+else:
+    print(f'Hey {uName} welcome to HTMLDigger world.')
